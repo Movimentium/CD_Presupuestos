e22a00c050a8cc27cf22345c5e0dfdf0059211d9
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-03-26 02:17:05 +0100

    CDModel added

--- CD_Presupuestos/CD_PresupuestosApp.swift
+++ CD_Presupuestos/CD_PresupuestosApp.swift
@@ -1,17 +1,16 @@
-//
 //  CD_PresupuestosApp.swift
 //  CD_Presupuestos
-//
 //  Created by Miguel Gallego on 26/3/25.
-//
-
 import SwiftUI
 
 @main
 struct CD_PresupuestosApp: App {
+    let provider = CDProvider()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, provider.moc)
         }
     }
 }
