0de794d87b7c75542ff0ecf8717c5945b1bb5271
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-03-26 01:47:03 +0100

    Initial Commit

---
+++ CD_Presupuestos/CD_PresupuestosApp.swift
@@ -0,0 +1,17 @@
+//
+//  CD_PresupuestosApp.swift
+//  CD_Presupuestos
+//
+//  Created by Miguel Gallego on 26/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CD_PresupuestosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CD_Presupuestos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CD_Presupuestos
+//
+//  Created by Miguel Gallego on 26/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
