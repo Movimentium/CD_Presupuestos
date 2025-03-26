b953bfb8d192f12fba381f0649068db1b7ef6d75
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-03-26 02:30:25 +0100

    Building UI

---
+++ CD_Presupuestos/Screens/PresupuestoScreen.swift
@@ -0,0 +1,31 @@
+//  PresupuestoScreen.swift
+//  CD_Presupuestos
+//  Created by Miguel Gallego on 26/3/25.
+import SwiftUI
+
+struct PresupuestoScreen: View {
+    @State private var isShowing = false
+    
+    var body: some View {
+        VStack {
+            Text("Aquí se mostrarán los presupuestos")
+        }
+        .navigationTitle("App Presupuestos")
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("Añadir Presupuesto") {
+                    isShowing = true
+                }
+            }
+        }
+        .sheet(isPresented: $isShowing) {
+            Text("AddPresupuestoScreen")
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        PresupuestoScreen()
+    }
+}
